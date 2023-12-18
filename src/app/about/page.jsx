f2e42fda1880f96077d84f4e8d09dbb672e27881
SamuelSantana17 <samuel.santana.966@my.csun.edu>
2023-12-17 21:43:14 -0800

    about page styling

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -4,38 +4,78 @@ import Image from "next/image";
 import React from "react";
 
 function About() {
-    const router = useRouter();
+  const router = useRouter();
 
-    return (
-        <div className="bg-black min-h-screen flex justify-center items-center p-4">
-            <div className="flex flex-col lg:flex-row justify-center items-center max-w-7xl mx-auto w-full">
-                <div className="w-3/4 lg:w-1/2 xl:w-1/3 mx-auto">
-                    <Image src='/logo_webDark.png' width={2000} height={2000} layout="responsive"/>
-                </div>
-                <div className="text-center lg:text-left mt-6 lg:mt-0 lg:ml-10">
-                    <h2 className="text-[24px] lg:text-[40px] font-semibold text-[#DC43FD]">About</h2>
-                    <p className="text-white text-[20px] lg:text-[16px]">
-        Welcome to InspoNet, the canvas of creativity and the nexus of
-        inspiration. Our journey began with a simple vision: to create a space
-        where imagination knows no bounds, and every idea finds a home. Here at
-        InspoNet, we believe that inspiration is the thread that weaves our
-        creativity together. It's a digital mosaic where artists, dreamers,
-        makers, and doers from across the globe connect, share, and grow. Our
-        platform is more than just a collection of images and ideas. It's a
-        vibrant community built on the foundation of collaboration,
-        encouragement, and shared passion. From the spark of an initial concept
-        to the final touches of a masterpiece, InspoNet is your partner in the
-        creative process. We're not just a social network; we're a support
-        network for all things imaginative. Dive into InspoNet and discover a
-        world of endless possibility. Pin your passions, curate your
-        collections, and watch as your inspirations ripple across our global
-        tapestry. Welcome to your new creative home.
-      </p>
-      <button onClick={() => router.push('/home')}
-      className="text-white hover:border-b border-[#DC43FD] cursor-pointer mt-10"><span>Get Started</span></button>
+  return (
+    <div className="bg-gradient-to-bl from-black to-[#F8EF74] via-[#DC43FD] min-h-screen flex justify-center items-center p-4">
+      <div className="flex flex-col  justify-center items-center max-w-7xl mx-auto w-full">
+        <div className="justify-center items-center flex">
+        <Image src='/logo_web.png' width={100} height={100}/>
+        <h1 className="ml-10 font-serif text-[60px] text-white ">InspoNet</h1>
+        </div>
+    
+        <div className="text-center lg:text-left mt-6 lg:mt-0 lg:ml-10">
+          <h2 className="text-[24px] font-serif lg:text-[40px] font-semibold text-[#F8EF74]">
+            About
+          </h2>
+          <p className="text-white text-[20px] lg:text-[16px]">
+            Welcome to InspoNet, the canvas of creativity and the nexus of
+            inspiration. Our journey began with a simple vision: to create a
+            space where imagination knows no bounds, and every idea finds a
+            home. Here at InspoNet, we believe that inspiration is the thread
+            that weaves our creativity together. It's a digital mosaic where
+            artists, dreamers, makers, and doers from across the globe connect,
+            share, and grow. Our platform is more than just a collection of
+            images and ideas. It's a vibrant community built on the foundation
+            of collaboration, encouragement, and shared passion. 
+            <br/>
+            <br/>
+            From the spark
+            of an initial concept to the final touches of a masterpiece,
+            InspoNet is your partner in the creative process. We're not just a
+            social network; we're a support network for all things imaginative.
+            Dive into InspoNet and discover a world of endless possibility. Pin
+            your passions, curate your collections, and watch as your
+            inspirations ripple across our global tapestry. Welcome to your new
+            creative home.
+          </p>
+          <button
+            onClick={() => router.push("/home")}
+            className="text-[#F8EF74] hover:border-b font-bold font-serif text-[20px] border-white cursor-pointer mt-10"
+          >
+            <span>Get Started</span>
+          </button>
+        </div>
+        <div className="text-center lg:text-left mt-6 lg:mt-0 lg:ml-10">
+          <h2 className="text-[24px] font-serif text-right lg:text-[40px] font-semibold text-[#F8EF74]">
+            Community
+          </h2>
+          <p className="text-white text-[20px] lg:text-[16px]">
+            At the heart of InspoNet lies our community, a tapestry of talent,
+            a fusion of fervor, and a symphony of supportive voices. It's where
+            creators meet collaborators, visionaries join volunteers, and
+            friends find fellow enthusiasts. Our community is your space to
+            shine, share, and support each other in every creative endeavor.
+            Here, every member is both a muse and a maven. Whether you're
+            looking to bounce around ideas, seek feedback on your latest
+            project, or just soak in the collective creativity, you've found
+            your tribe. Our forums buzz with activity, our groups glow with
+            shared interests, and our collaborations brim with collective
+            genius. Join our community events, participate in challenges, or
+            start your own group. 
+            <br/>
+            <br/>
+            InspoNet is your platform to foster
+            connections that transcend the digital realm. Together, we're not
+            just building a network; we're weaving a world where every
+            inspiration is interlinked. Engage, empower, and elevate each other
+            because on InspoNet, every connection counts, every share
+            inspires, and every individual is an integral thread in the fabric
+            of our community. Welcome to where your presence is valued and your
+            creativity cherished. Welcome to InspoNet.
+          </p>
+        </div>
       </div>
-      
-    </div>
     </div>
   );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,34 +6,30 @@ import React from 'react';
 export default function LandingPage() {
   const router = useRouter();  
   return (
-    <div className='bg-gradient-radial w-screen h-screen to-[#F8EF74] from-[#DC43FD] flex flex-col justify-center items-center'>
-      <div className='flex justify-center items-center'>
-        <div className='p-4 rounded-lg bg-white w-[300px] h-[300px] flex justify-center items-center mr-10'>
-          <Image src='/logo_web.png' width={300} height={300}/>
+    <div className='bg-gradient-to-br w-screen h-screen to-[#F8EF74] from-[#DC43FD] flex flex-col justify-center items-center px-4'>
+      <div className='flex flex-col md:flex-row justify-center items-center text-center md:text-left'>
+        <div className='p-4 rounded-lg bg-white w-full max-w-[300px] h-auto flex justify-center items-center mb-6 md:mb-0 md:mr-10'>
+          <Image src='/logo_web.png' width={300} height={300} layout="responsive"/>
         </div>
         <div>
-          <h1 className='text-white text-[90px]'>Name</h1> 
+          <h1 className='text-white font-serif text-[60px] md:text-[90px]'>InspoNet</h1> 
+          <h2 className='text-white font-serif text-[25px] md:text-[40px]'>Where Ideas Connect</h2>
           <button onClick={() => router.push('/home')}
-          className='text-white text-[40px] hover:border-b cursor-pointer border-white'>Get Started</button>
+            className='text-white  font-serif text-[20px] md:text-[30px] mt-6 md:mt-10 hover:border-b cursor-pointer border-white'>Get Started</button>
         </div>
       </div>
-      <div className='mt-40 flex justify-center space-x-4'> {/* Use flex and space-x-4 to align items side by side and give them some space */}
-       
-       <button onClick={() => router.push('/about')} 
-       className='text-white text-[15px] hover:border-b cursor-pointer border-white'
-       ><span>About</span></button>
-       
-       <button onClick={() => router.push('/about')} 
-       className='text-white text-[15px] hover:border-b cursor-pointer border-white'
-       ><span>Community</span></button>
-       
-       <button onClick={() => router.push('/about')} 
-       className='text-white text-[15px] hover:border-b cursor-pointer border-white'
-       ><span>Careers</span></button>
-       
-       
-       
-       
+      <div className='flex flex-col md:flex-row justify-center space-y-4 md:space-y-0 md:space-x-4 mt-20 md:mt-40'>
+        <button onClick={() => router.push('/about')} 
+          className='text-white text-[15px] hover:border-b cursor-pointer border-white'
+        ><span>About</span></button>
+        
+        <button onClick={() => router.push('/community')} 
+          className='text-white text-[15px] hover:border-b cursor-pointer border-white'
+        ><span>Community</span></button>
+        
+        <button onClick={() => router.push('/careers')} 
+          className='text-white text-[15px] hover:border-b cursor-pointer border-white'
+        ><span>Careers</span></button>
       </div>
     </div>
   );
